33eff7e3fe8d068932899555114fdfa8e54cc9dc
Rose-Alves <rosyalves74@hotmail.com>
2022-08-30 11:22:50 -0300

    estudando

---
+++ inicio/second.js
@@ -0,0 +1,15 @@
+{
+  console.log("nunber 1");
+  console.log("nunber 3");
+  console.log("nunber 5");
+  console.log("nunber 7");
+  console.log("nunber 9");
+}
+{
+  console.log("nunber 2");
+  console.log("nunber 4");
+  console.log("nunber 6");
+  console.log("nunber 8");
+  console.log("nunber 10");
+}
+// temos acima 5 sentenças de codigo console.log(""); em dois blocos { }
