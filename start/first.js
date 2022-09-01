9aad99eec9aed216d4d27cae1446e68b329d7ad5
Rose-Alves <rosyalves74@hotmail.com>
2022-09-01 09:59:50 -0300

    update

--- start/first.js
+++ start/first.js
@@ -1,15 +1,17 @@
 //  isso é um comentário, console.log é usado para exibri algo na tela do computador!
-/* este é um comentário de multplas linhas no js. */ /*  linha 4 quantidade valor literl inteiro 10, 3,4 não é quebrado ex: 6.4*/
+/* este é um comentário de multplas linhas no js. */ /*  linha 4 quantidade valor literl inteiro 10, 3,4 não é quebrado ex: 6.4
+var variavél armazena informação*/
 
-let nome = "Caneta";
-let quantidade = 10;
-let preco = 6.4;
+let nome = "Canetas"; // ; ponto e virgula por ser uma centença de codigo.
+let quantidade = 10; // = simbolo de atribuição.
+let preco = 6.4; // no js usa-se o padrão americano separa-se as casas decimais com . não virgula ex; 6.4
 let imposto = 1.5;
 let precoFinal = preco + imposto;
+// mais de uma palavra para definir a variavél nos temos que sempre começar a segunda palavra com letra maiuscula ex; precoFinalTotal.
 
 console.log(nome);
 console.log(quantidade);
-console.log(preco);
+console.log(preco); //nomes sem acentuação.
 console.log(imposto);
 console.log(precoFinal);
 
