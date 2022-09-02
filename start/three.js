917bfdf2fd8dacff3270abaafcbedafa61f5173a
Rose-Alves <rosyalves74@hotmail.com>
2022-09-02 11:24:31 -0300

    done today

---
+++ start/three.js
@@ -0,0 +1,22 @@
+console.log(19.9 * 0.6); // let palavra reservada da variável
+
+let preco = 19.9; // = recebe
+let desconto = 0.4; // 0.4 igual 40% de desconto
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
+
+let nome = " Caderno "; // String -> sequencia de símbolos ex: $%?1f
+let categoria = " Papelaria ";
+
+console.log(
+  " Produto: " +
+    nome +
+    ", Categoria: " +
+    categoria +
+    " Preço:" +
+    preco +
+    ", Desconto: " +
+    desconto
+);
+
+// console.log ( 1+1); números então ele somará ficara 2 porém ("1" + 1);isso já é texto. então ele juntará ficará 11.
